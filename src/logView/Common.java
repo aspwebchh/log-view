c1c624a0de2d507d031bbebc865bfa5cea58ec15
CHENHONGHONG\宏鸿 <aspwebchh@hotmail.com>
2018-09-28 12:13:44 +0800

    增加查看日志对话框

--- src/logView/Common.java
+++ src/logView/Common.java
@@ -1,6 +1,12 @@
 package logView;
 
 import javafx.scene.control.Alert;
+import javafx.scene.control.TextArea;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.Priority;
+
+import java.io.PrintWriter;
+import java.io.StringWriter;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
@@ -10,6 +16,34 @@ public class Common {
         Alert alert = new Alert(Alert.AlertType.ERROR);
         alert.setContentText(msg);
         alert.setHeaderText("");
+        alert.showAndWait();
+    }
+
+    public static void showContent(String data) {
+        Alert alert = new Alert(Alert.AlertType.ERROR);
+        alert.setTitle("日志内容");
+        alert.setHeaderText("日志内容");
+
+        StringWriter sw = new StringWriter();
+        PrintWriter pw = new PrintWriter(sw);
+        pw.print(data);
+        String exceptionText = sw.toString();
+
+        TextArea textArea = new TextArea(exceptionText);
+        textArea.setEditable(false);
+        textArea.setWrapText(true);
+
+        textArea.setMaxWidth(Double.MAX_VALUE);
+        textArea.setMaxHeight(Double.MAX_VALUE);
+        GridPane.setVgrow(textArea, Priority.ALWAYS);
+        GridPane.setHgrow(textArea, Priority.ALWAYS);
+
+        GridPane expContent = new GridPane();
+        expContent.setMaxWidth(Double.MAX_VALUE);
+        expContent.add(textArea, 0, 1);
+
+        alert.getDialogPane().setExpandableContent(expContent);
+        alert.getDialogPane().setExpanded(true);
         alert.showAndWait();
     }
 
--- src/logView/LogView.java
+++ src/logView/LogView.java
@@ -2,27 +2,33 @@ package logView;
 
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.concurrent.Task;
 import javafx.fxml.FXML;
-import javafx.scene.control.ComboBox;
+import javafx.scene.control.*;
 import javafx.scene.control.Label;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableView;
+import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.control.cell.TextFieldTableCell;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.Priority;
+import javafx.util.Callback;
 import logView.Common;
 import logView.LogItem;
 import logView.LogUtil;
 import logView.RangeQueryControlItem;
 
 import java.awt.*;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.io.StringWriter;
 import java.util.List;
 
 public class LogView {
 
     @FXML
-    private ComboBox dir, range,type;
+    private ComboBox dir, range, type;
     @FXML
     private TableView log_table;
     @FXML
@@ -55,10 +61,20 @@ public class LogView {
         log_table.prefWidthProperty().bind(log_body.widthProperty());
         log_table.prefHeightProperty().bind(log_body.heightProperty().subtract(60));
 
-        ObservableList<TableColumn<?,?>> cols = log_table.getColumns();
+        ObservableList<TableColumn<?, ?>> cols = log_table.getColumns();
         TableColumn contentCol = cols.get(2);
         contentCol.prefWidthProperty().bind(log_table.widthProperty().subtract(250));
-        contentCol.setCellFactory(TextFieldTableCell.forTableColumn());
+        //contentCol.setCellFactory(TextFieldTableCell.forTableColumn());
+        log_table.setRowFactory(tv -> {
+            TableRow row = new TableRow();
+            row.setOnMouseClicked(event -> {
+                if (event.getClickCount() == 2 && (!row.isEmpty())) {
+                    Object rowData = row.getItem();
+                    Common.showContent(rowData.toString());
+                }
+            });
+            return row;
+        });
     }
 
     @FXML
@@ -69,21 +85,20 @@ public class LogView {
         initTable();
     }
 
-
     @FXML
     public void click() {
         String selectDir = (String) dir.getValue();
-        RangeQueryControlItem selectRange = (RangeQueryControlItem)range.getValue();
+        RangeQueryControlItem selectRange = (RangeQueryControlItem) range.getValue();
         String selectType = (String) type.getValue();
         String sKeyword = keyword.getText().trim();
         String sExceptKeyword = except_keyword.getText().trim();
         String sRegex = regex.getText();
-        if(selectDir == null) {
+        if (selectDir == null) {
             Common.showAlert("未选择目录");
             return;
         }
-        List<LogItem> logItems = LogUtil.getLogContent(selectDir, selectRange.value, selectType,sKeyword,sExceptKeyword,sRegex);
+        List<LogItem> logItems = LogUtil.getLogContent(selectDir, selectRange.value, selectType, sKeyword, sExceptKeyword, sRegex);
         log_table.setItems(FXCollections.observableArrayList(logItems));
-        log_count.setText( logItems.size() + "个项目");
+        log_count.setText(logItems.size() + "个项目");
     }
 }
